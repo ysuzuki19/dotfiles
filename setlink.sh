41c75c45698804cf7dcfaea83d23a06de73fa55b
ysuzuki19 <msethuuh7@i.softbank.jp>
2019-03-28 17:23:06 +0900

    set symbolic link shell script

---
+++ setlink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -sf ~/Github/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/Github/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/Github/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/Github/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Github/dotfiles/.tmux.conf ~/.tmux.conf
+source ~/.bashrc
+source ~/.bash_profile
+source ~/.inputrc
